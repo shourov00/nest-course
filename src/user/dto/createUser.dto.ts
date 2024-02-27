d88f9c3c8b470e61a0394604dbc6392cb3518c36
Shourov00 <sfazleyrabbi@gmail.com>
2024-02-27 22:24:35 +0600

    user services

---
+++ src/user/dto/createUser.dto.ts
@@ -0,0 +1,12 @@
+import { IS_EMAIL, IsEmail, IsNumberString, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNumberString()
+  phone: string;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  findAll(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
